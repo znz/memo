b9052d7166c230adc793563242cb109cee923441
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2022-03-17 14:34:37 +0900

    Add `Apt::Get::Purge 1`

--- 2022/init-in-lima.sh
+++ 2022/init-in-lima.sh
@@ -11,6 +11,10 @@ if [ -d /etc/needrestart/conf.d ]; then
     echo "\$nrconf{restart} = 'a';" | sudo tee /etc/needrestart/conf.d/local.conf
 fi
 
+if [ ! -f /etc/apt/apt.conf.d/50local ]; then
+    echo 'APT::Get::Purge 1;' | sudo tee /etc/apt/apt.conf.d/50local
+fi
+
 sudo apt update
 #apt list --upgradable
 sudo apt install -y etckeeper
