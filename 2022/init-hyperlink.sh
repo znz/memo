9bba3f8e1473862b94fa169c55af53b2a9c41571
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2022-03-17 14:25:44 +0900

    Fix error

--- 2022/init-hyperlink.sh
+++ 2022/init-hyperlink.sh
@@ -2,6 +2,7 @@
 set -euxo pipefail
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cd $HOME
 git clone https://github.com/untitaker/hyperlink
 cd hyperlink/
 cargo build --release
