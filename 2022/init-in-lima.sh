b5b9b369bc7f89e087f046f5698624c58b04d10a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2022-05-01 10:00:48 +0900

    Remove resolved workaround comment

--- 2022/init-in-lima.sh
+++ 2022/init-in-lima.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-# https://github.com/lima-vm/lima/issues/712
-#if [ aarch64 = "$(uname -m)" ]; then
-#    sudo apt-mark hold linux-image-$(uname -r) linux-headers-generic linux-headers-virtual linux-image-virtual linux-virtual
-#fi
-#sudo apt-mark unhold linux-image-$(uname -r) linux-headers-generic linux-headers-virtual linux-image-virtual linux-virtual
-
 if [ -d /etc/needrestart/conf.d ]; then
     echo "\$nrconf{restart} = 'a';" | sudo tee /etc/needrestart/conf.d/50local.conf
 fi
