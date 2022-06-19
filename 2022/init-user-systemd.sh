1a3d9a786a363cc0148103936400bbbb4decfd4d
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2022-06-19 22:38:13 +0900

    Add example units

--- 2022/init-user-systemd.sh
+++ 2022/init-user-systemd.sh
@@ -3,3 +3,33 @@ set -euxo pipefail
 
 sudo loginctl enable-linger $USER
 mkdir -p ~/.config/systemd/user
+
+cat > ~/.config/systemd/user/example.service <<EOF
+[Unit]
+Description=Example Service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/ruby -e 'STDOUT.puts :out; STDERR.puts :err'
+EOF
+
+cat > ~/.config/systemd/user/example.timer <<EOF
+[Unit]
+Description=Example Timer
+
+[Timer]
+OnCalendar=*-*-* *:55
+Persistent=true
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl --user enable example.timer --now
+
+systemctl --user status example.service || true
+systemctl --user status example.timer || true
+systemctl --user list-timers
+# sudo adduser $USER systemd-journal
+journalctl --user-unit example.service --no-pager
+journalctl --user-unit example.timer --no-pager
