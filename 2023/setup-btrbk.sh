00563e828f1dfc0d7f0e99d87ea3c9acdae9eba8
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2023-07-26 10:05:24 +0900

    Setup btrbk after create-vm.sh

---
+++ 2023/setup-btrbk.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euxo pipefail
+name=${1?$0 name}
+time limactl shell $name sudo btrfs subvolume create /mnt/btr_pool/_btrbk_snap || :
+time limactl shell $name sudo DEBIAN_FRONTEND=noninteractive apt install -y btrbk
+time limactl shell $name sudo tee /etc/btrbk/btrbk.conf <<EOF
+transaction_log            /var/log/btrbk.log
+timestamp_format           long-iso
+stream_buffer              256m
+snapshot_dir               _btrbk_snap
+
+snapshot_preserve_min   2d
+snapshot_preserve       14d
+
+volume /mnt/btr_pool
+  subvolume @
+  subvolume @home
+    snapshot_preserve 14d 4w 6m
+  subvolume @var_log
+    snapshot_preserve 7d 4w
+  subvolume @*tmp
+    snapshot_preserve 7d
+    snapshot_create onchange
+  subvolume @var_cache
+    snapshot_preserve 7d
+    snapshot_create onchange
+  subvolume @*snap*
+    group snapd
+    snapshot_preserve 7d
+    snapshot_create onchange
+EOF
+time limactl shell $name sudo tee /etc/apt/apt.conf.d/70btrbk <<'EOF'
+// create a btrfs snapshot before (un)installing packages
+Dpkg::Pre-Invoke  {"if [ -x /usr/bin/btrbk ]; then /usr/bin/btrbk --preserve run /mnt/btr_pool/@; fi"; };
+DPkg::Post-Invoke {"if [ -x /usr/bin/btrbk ]; then /usr/bin/btrbk --preserve run /mnt/btr_pool/@; fi"; };
+EOF
+time limactl shell $name sudo systemctl enable --now btrbk.timer
+time limactl shell $name sudo etckeeper commit "Setup btrbk"
+time limactl shell $name sudo systemctl start btrbk.service
+time limactl shell $name sudo journalctl -u btrbk.service --no-pager
