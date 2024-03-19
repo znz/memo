10ec9af4af4f1137d5679f402fc977cf6f28a4fb
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2024-03-19 15:40:15 +0900

    Test on macOS

--- 2024/riscv64/run.sh
+++ 2024/riscv64/run.sh
@@ -1,14 +1,55 @@
 #!/bin/bash
 set -euxo pipefail
 if [ ! -f ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img.xz ]; then
-	    wget -N https://cdimage.ubuntu.com/releases/22.04.4/release/ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img.xz
+    curl -LO https://cdimage.ubuntu.com/releases/22.04.4/release/ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img.xz
 fi
 if [ ! -f ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img ]; then
-	    xz -dk ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img.xz
-	        qemu-img resize -f raw ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img +5G
+    xz -dk ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img.xz
+    qemu-img resize -f raw ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img +5G
 fi
-if [ ! -f metadata.yaml ]; then
-	    echo "instance-id: $(uuidgen || echo i-abcdefg)" > metadata.yaml
+img=ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img
+if [ ! -f meta-data.yaml ]; then
+    echo "instance-id: $(uuidgen || echo i-abcdefg)" > meta-data.yaml
 fi
-cloud-localds my-seed.img user-data.yaml metadata.yaml
-qemu-system-riscv64 -machine virt -nographic -m 2048 -smp 4 -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.bin -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf -device virtio-net-device,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-rng-pci -drive file=ubuntu-22.04.4-preinstalled-server-riscv64+unmatched.img,format=raw,if=virtio -drive if=virtio,format=raw,file=my-seed.img
+cidata=my-seed.img
+if [ -x /usr/bin/cloud-localds ]; then
+    cloud-localds "$cidata" user-data.yaml meta-data.yaml
+fi
+
+# うまくいかなかったので、Ubuntu で生成した my-seed.img を持ってきた。
+if false; then
+    mkdir -p config
+    cp metadata.yaml config/meta-data
+    cp user-data.yaml config/user-data
+    cidata=cidata.iso
+    rm "$cidata"
+    # -o my-seed.img だと勝手に .iso がついて my-seed.img.iso になる。
+    # config/ ディレクトリが ISO の中のルートになる。
+    # cidata が CIDATA になってしまった。
+    hdiutil makehybrid -o "$cidata" -hfs -joliet -iso -default-volume-name cidata config/
+    rm -rf config
+fi
+
+fw=/usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.bin
+if [ ! -f "$fw" ]; then
+    fw=$(echo /opt/homebrew/Cellar/qemu/*/share/qemu/opensbi-riscv64-generic-fw_dynamic.bin)
+fi
+uboot=/usr/lib/u-boot/qemu-riscv64_smode/uboot.elf
+# riscv64 用の uboot は homebrew の qemu に入っていなかったのでコピーしてきた。
+if [ -f uboot.elf ]; then
+    uboot=uboot.elf
+fi
+
+args=(
+    -machine virt
+    -nographic
+    -m 2048
+    -smp 4
+    -bios "$fw"
+    -kernel "$uboot"
+    -device "virtio-net-device,netdev=net0" -netdev "user,id=net0,hostfwd=tcp::2222-:22"
+    -device virtio-rng-pci
+    -drive "if=virtio,format=raw,file=$img"
+    -drive "if=virtio,format=raw,file=$cidata"
+)
+qemu-system-riscv64 "${args[@]}"
