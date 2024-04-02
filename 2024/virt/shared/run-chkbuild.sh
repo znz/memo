50326a385a9bb423537f1dcd50290b255228a96f
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2024-04-02 18:12:21 +0900

    Disable s3_upload_target

--- 2024/virt/shared/run-chkbuild.sh
+++ 2024/virt/shared/run-chkbuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+sudo sed -i -e 's/^ChkBuild.s3_upload_target/# ChkBuild.s3_upload_target/' /home/chkbuild/chkbuild/start-rubyci
+
 exec sudo systemd-run \
      --uid=chkbuild --gid=chkbuild \
      -E RUBYCI_NICKNAME=debian-riscv64 \
